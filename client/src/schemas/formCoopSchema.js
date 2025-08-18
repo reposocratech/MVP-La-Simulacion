c06f2334ea7c7a167745c49f4743ac0698e39c7a
Alberto <pendientintin@hotmail.com>
2025-08-18 13:27:08 +0200

    servicios coop funcionalidad

---
+++ client/src/schemas/formCoopSchema.js
@@ -0,0 +1,31 @@
+import {z, ZodError} from 'zod';
+
+export const formCoopSchema = z.object({
+    user_name: z
+        .string({message:"El nombre es necesario"})
+        .min(3,{message:"El nombre debe ser mayor de 3 caracteres"})
+        .max(30,{message:"El nombre debe ser menor de 30 caracteres"}),
+
+    lastName: z
+    .string({message:"El apellido es necesario"})
+    .min(2,{message:"El apellido debe ser mayor de 2 caracteres"})
+    .max(30,{message:"El apellido debe ser menor de 30 caracteres"}),
+
+    email: z
+    .email({message:"Email no válido"}),
+
+    phone: z
+    .string({message:"El teléfono es necesario"})
+    .min(7, "El número es demasiado corto")
+    .max(15, "El número es demasiado largo")
+    .regex(/^\d+$/, "El teléfono solo debe contener números"),
+
+    type: z
+    .string({message:"Debes marcar alguna de las opciones"}),
+
+    description: z
+    .string({message:"El descripción es necesaria"})
+    .min(10,{message:"La descripción tiene que ser mas larga"})
+    .max(500,{message:"El descripción debe ser menor de 500 caracteres"}),
+   
+})
