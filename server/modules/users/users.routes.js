a85c2dc810cbb536cdee0cf05bfca7cb2f3c6a58
Alberto <pendientintin@hotmail.com>
2025-08-19 08:27:57 +0200

    Cambios de view del register

--- server/modules/users/users.routes.js
+++ server/modules/users/users.routes.js
@@ -3,11 +3,12 @@ import usersController from './users.controllers.js';
 import { verifyToken } from '../../middlewares/verifyToken.js';
 import { validateForm } from '../../middlewares/validateForm.js';
 import { loginSchema } from '../../schemas/loginSchema.js';
+import { registerSchema } from '../../schemas/registerSchema.js';
 
 const router = express.Router();
 
 //Rutas Publicas
-router.post('/register', usersController.register);
+router.post('/register', validateForm(registerSchema) , usersController.register);
 router.get('/verify-email', usersController.verifyEmail);
 router.post('/login', validateForm(loginSchema), usersController.login);
 router.get('/userById', verifyToken, usersController.userById);
