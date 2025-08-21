ba462f3830c11e11b3873f630dd749dd408e2953
Fernando Fuentes <fernando05fuentes07@gmail.com>
2025-08-21 14:19:03 +0200

    vista perfil

--- client/src/context/AuthContextProvider.jsx
+++ client/src/context/AuthContextProvider.jsx
@@ -1,67 +1,72 @@
-import { createContext, useEffect, useState } from "react";
-import { fetchData } from "../helpers/axiosHelper";
+import { createContext, useEffect, useState } from 'react'
+import { fetchData } from '../helpers/axiosHelper'
 
-export const AuthContext = createContext();
+export const AuthContext = createContext()
 
-export const AuthContextProvider = ({children}) => {
-  const [user, setUser] = useState();
-  const [token, setToken] = useState();
-  const [loading, setLoading] = useState(true);
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState()
+  const [token, setToken] = useState()
+  const [loading, setLoading] = useState(true)
 
-  useEffect(()=>{
-    let tokenLS = localStorage.getItem("token")
-    if(tokenLS){
-      const fetching = async()=>{
+  useEffect(() => {
+    let tokenLS = localStorage.getItem('token')
+    if (tokenLS) {
+      const fetching = async () => {
         try {
-          const res = await fetchData("/users/userById", "get", null, tokenLS);
-          setUser(res.data.user);
+          const res = await fetchData('/users/userById', 'get', null, tokenLS)
+          setUser(res.data.user)
           setToken(tokenLS)
         } catch (error) {
-          console.log(error);
+          console.log(error)
         } finally {
-          setLoading(false);
+          setLoading(false)
         }
       }
-      fetching();
-    }else{
-      setLoading(false);
+      fetching()
+    } else {
+      setLoading(false)
     }
-  },[]);
+  }, [])
 
-  const login = async(userLogin)=> {
+  const login = async (userLogin) => {
     try {
-      let res = await fetchData('/users/login', "post", userLogin);
-      
-      let tokenBack = res.data.token;
+      let res = await fetchData('/users/login', 'post', userLogin)
 
-      const responseUser = await fetchData("/users/userById", "get", null, tokenBack);
-      localStorage.setItem("token", tokenBack);
-      setToken(tokenBack);
-      setUser(responseUser.data.user);
+      let tokenBack = res.data.token
 
+      const responseUser = await fetchData(
+        '/users/userById',
+        'get',
+        null,
+        tokenBack
+      )
+      localStorage.setItem('token', tokenBack)
+      setToken(tokenBack)
+      setUser(responseUser.data.user)
     } catch (error) {
-      throw error;
+      throw error
     }
+  }
 
-  };
-
-  const logout = () =>{
-    localStorage.removeItem("token");
-    setUser();
-    setToken();
-  };
+  const logout = () => {
+    localStorage.removeItem('token')
+    setUser()
+    setToken()
+  }
 
   return (
     <>
-      <AuthContext.Provider value={{
-        user,
-        setUser,
-        token,
-        setToken,
-        login,
-        logout,
-        loading
-      }}>
+      <AuthContext.Provider
+        value={{
+          user,
+          setUser,
+          token,
+          setToken,
+          login,
+          logout,
+          loading,
+        }}
+      >
         {children}
       </AuthContext.Provider>
     </>
