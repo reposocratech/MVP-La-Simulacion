88d060b0d9552437e4f9ba2861bd01c3e0f0ed0f
Amaia <amaiavzqz@gmail.com>
2025-08-22 08:51:51 +0200

    maquetacion room, eventos y comienzo de about

---
+++ client/src/components/Cards/cardEvents/CardEvents.jsx
@@ -0,0 +1,24 @@
+import './cardEvents.css';
+
+export const CardEvents = ({event}) => {
+  return (
+    <article className='card rounded-4 overflow-hidden'>
+        <div className="row ">
+          <div className="col-lg-7">
+            <div className="p-3 d-flex flex-column justify-content-between h-100">
+              <h4 className='mb-3'>{event.title}</h4>
+              <p>
+                {event.description}
+              </p>
+              <div>
+                <button className='info-button'>Mas información</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-5">
+            <img src={event.cover_image} alt="" className='w-100 h-100 object-fit-cover'/>
+          </div>
+        </div>
+    </article>
+  )
+}
