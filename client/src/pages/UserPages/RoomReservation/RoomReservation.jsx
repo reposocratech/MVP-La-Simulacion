6460c0705ad989c267d4128f1bece1831d041161
Laura Sánchez <laezne@gmail.com>
2025-08-24 18:07:03 +0200

    Protección de las rutas privadas, pruebo asincronía en las funciones de onLogout de los Navbar

--- client/src/pages/UserPages/RoomReservation/RoomReservation.jsx
+++ client/src/pages/UserPages/RoomReservation/RoomReservation.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Col, Container, Form, Row } from "react-bootstrap";
 import { AuthContext } from "../../../context/AuthContextProvider";
+import './roomReservation.css';
 
 const RoomReservation = () => {
 
@@ -9,15 +10,19 @@ const RoomReservation = () => {
   return (
     <section className="section-reservation">
       <Container>
-        <Row>
-          <Col>
-            <Form>
+        <h1 className="text-center">
+        <span className="accent-text spanLetter-reservation">PA</span>
+          Solicitud de Reserva
+        </h1>
+        <Row className="d-flex justify-content-center">
+          <Col xs={12} lg={6}>
+            <Form className="form-register border border-2 rounded rounded-3 mb-2">
               <Form.Group className="mb-3" controlId="formPlaintextName">
                 <Form.Label>Nombre completo:</Form.Label>
                 <Form.Control 
                         plaintext 
                         readOnly 
-                        defaultValue="Hola" />
+                        defaultValue={user.user_name} />
               </Form.Group>
 
               
--- client/src/routes/PrivateRoutes.jsx
+++ client/src/routes/PrivateRoutes.jsx
@@ -1,10 +1,24 @@
-import { Outlet } from "react-router"
+import { useContext } from "react";
+import { Outlet, useNavigate } from "react-router"
+import { AuthContext } from "../context/AuthContextProvider";
+import { useEffect } from "react";
 
 
-export const PrivateRoutes = () => {
+export const PrivateRoutes = ({userType, requiredUser}) => {
+
+  const navigate = useNavigate();
+
+  const {user} = useContext(AuthContext);
+
+  useEffect(() => {
+    if (userType !== requiredUser){
+      navigate("/");
+    }
+  }, [user]);
+
   return (
     <>
-      <Outlet />
+      {user && <Outlet />}
     </>
   )
 }
