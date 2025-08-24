2f2946bb56c2bac4a0b9413c57b8882a77a28d28
Laura Sánchez <laezne@gmail.com>
2025-08-24 22:34:47 +0200

    Creación de los 3 ReservationForms

---
+++ client/src/components/ReservationForms/ReservationForm1.jsx
@@ -0,0 +1,80 @@
+import { Col, Form, Row } from "react-bootstrap";
+
+
+export const ReservationForm1 = ({userName, userLastname, handleChange, goNext, cancel}) => {
+
+  return (
+    <Form className="border border-2 rounded rounded-3 mb-2 mt-3 p-4">
+      <Form.Group className="mb-3" controlId="formBasicRoom">
+        <Form.Label>Espacio a reservar:</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Set de Grabación"
+          aria-label="Sala a reservar"
+          disabled
+          readOnly
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicFullName">
+        <Form.Label>Tu nombre:</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder={`${userName} ${userLastname ? userLastname : ""}`}
+          aria-label="Nombre completo que aparece en tu perfil"
+          disabled
+          readOnly
+        />
+      </Form.Group>
+      <Form.Group as={Row} className="mb-3" controlId="formBasicPhone">
+        <Form.Label column sm="5">Teléfono de contacto:</Form.Label>
+        <Col sm={7}>
+          <Form.Control
+            type="text"
+            placeholder="Número de teléfono"
+            onChange={handleChange}
+          />
+        </Col>
+      </Form.Group>
+      <Form.Group as={Row} className="mb-3" controlId="formBasicDate">
+        <Form.Label column sm="5">Fecha solicitada:</Form.Label>
+        <Col sm={7}>
+          <Form.Control
+            type="date"
+            onChange={handleChange}
+          />
+        </Col>
+      </Form.Group>
+      <Form.Group as={Row} className="mb-3" controlId="formBasicStartHour">
+        <Form.Label column sm="5">Hora de inicio:</Form.Label>
+        <Col sm={7}>
+          <Form.Control
+            type="time"
+            onChange={handleChange}
+          />
+        </Col>
+      </Form.Group>
+      <Form.Group as={Row} className="mb-4" controlId="formBasicEndHour">
+        <Form.Label column sm="5">Hora de fin:</Form.Label>
+        <Col sm={7}>
+          <Form.Control
+            type="time"
+            onChange={handleChange}
+          />
+        </Col>
+      </Form.Group>
+      <div className="d-flex gap-2">
+        <button 
+            className="cancel-button"
+            onClick={cancel}
+        >Cancelar
+        </button>
+        <button 
+            className="submit-button"
+            type="button"
+            onClick={(e)=>goNext(e, 1)}
+            >Siguiente
+        </button>
+      </div>
+    </Form>
+  )
+}
--- client/src/pages/UserPages/RoomReservation/RoomReservation.jsx
+++ client/src/pages/UserPages/RoomReservation/RoomReservation.jsx
@@ -1,36 +1,62 @@
-import { useContext } from "react";
-import { Col, Container, Form, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import { ReservationForm1 } from "../../../components/ReservationForms/ReservationForm1";
+import { useContext, useState } from "react";
+import { useNavigate } from 'react-router';
 import { AuthContext } from "../../../context/AuthContextProvider";
+import { ReservationForm2 } from "../../../components/ReservationForms/ReservationForm2";
+import { ReservationForm3 } from "../../../components/ReservationForms/ReservationForm3";
 import './roomReservation.css';
 
+
 const RoomReservation = () => {
+  const [reservationData, setReservationData] = useState();
+  const [showForm, setShowForm] = useState(1);
 
   const {user} = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleChange = () => {
+
+  }
+
+  const goNext = (e, numberForm) => {
+    e.preventDefault();
+    setShowForm(numberForm + 1);
+  }
+
+  const cancel = (e) => {
+    e.preventDefault();
+    setReservationData();
+    navigate("/oneRoom/2");
+  }
 
   return (
     <section className="section-reservation">
       <Container>
-        <h1 className="text-center">
-        <span className="accent-text spanLetter-reservation">PA</span>
+        <h1 className="h1-reservation text-center">
+        <span className="accent-text spanLetter-reservation">SR</span>
           Solicitud de Reserva
         </h1>
         <Row className="d-flex justify-content-center">
           <Col xs={12} lg={6}>
-            <Form className="form-register border border-2 rounded rounded-3 mb-2">
-              <Form.Group className="mb-3" controlId="formPlaintextName">
-                <Form.Label>Nombre completo:</Form.Label>
-                <Form.Control 
-                        plaintext 
-                        readOnly 
-                        defaultValue={user.user_name} />
-              </Form.Group>
-
-              
-              <div>
-                <button className="cancel-button">Cancelar</button>
-                <button className="submit-button">Siguiente</button>
-              </div>
-            </Form>
+            {showForm === 1 &&
+              <ReservationForm1 
+                userName={user.user_name}
+                userLastname={user.lastname}
+                handleChange={handleChange}
+                goNext={goNext}
+                cancel={cancel}/>
+            }
+            {showForm === 2 &&
+              <ReservationForm2
+                goNext={goNext} 
+                cancel={cancel}/>
+            }
+            {showForm === 3 &&
+              <ReservationForm3 
+                cancel={cancel}/>
+            }
           </Col>
         </Row>
       </Container>
