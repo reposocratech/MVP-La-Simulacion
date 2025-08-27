587e927b35801c6ab1057302e8b2124eca21ee4b
Amaia <amaiavzqz@gmail.com>
2025-08-27 16:42:26 +0200

    pagina servicios corregida

--- client/src/components/Cards/cardServicios/CardServicios.jsx
+++ client/src/components/Cards/cardServicios/CardServicios.jsx
@@ -5,21 +5,19 @@ export const CardServicios = ({service}) => {
   const navigate = useNavigate();
 
   return (
-    <article className='card-servicios p-3 rounded-4 overflow-hidden' style={{ backgroundColor: service.bgColor }}>
-      <div className='mb-3'>
-        <img src={service.img} alt=""  className="w-100 rounded-4"/>
-      </div>
-      <div>
+    <article className='card-servicios p-3 rounded-4 overflow-hidden h-100' style={{ backgroundColor: service.bgColor }}>
+      <div className='d-flex flex-column justify-content-between h-100'>
+        <div className='mb-3'>
+          <img src={service.img} alt=""  className="w-100 rounded-4"/>
+        </div>
         <h4 className='mb-3'><span>{service.span}</span>{service.title}</h4>
-        <p className='mb-1'>{service.p1}</p>
-        <p className='mb-1'>{service.p2}</p>
-        <p className='mb-1'>{service.p3}</p>
-        <p className='mb-1'>{service.p4}</p>
-        <p className='mb-1'>{service.p5}</p>
-        <p className='mb-1'>{service.p6}</p>
-      </div>
-      <div className='mt-3'>
-        <button 
+        <p>{service.p1}</p>
+        <p>{service.p2}</p>
+        <p>{service.p3}</p>
+        <p>{service.p4}</p>
+        <p>{service.p5}</p>
+        <p>{service.p6}</p>
+        <button
           className='service-button w-100'
           onClick={() => navigate(service.path)}
         >{service.button}</button>
