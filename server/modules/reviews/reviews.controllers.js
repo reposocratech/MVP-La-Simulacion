4c07bb6789e2e0ad84ee9ea7b9e683d69485468d
Amaia <amaiavzqz@gmail.com>
2025-08-29 17:45:16 +0200

    avance en reviews

--- server/modules/reviews/reviews.controllers.js
+++ server/modules/reviews/reviews.controllers.js
@@ -3,11 +3,11 @@ import reviewsDal from "./reviews.dal.js";
 class ReviewController {
   createReview = async(req, res) => {
     try {
-      const { comment, rating } = req.body;
-      await reviewsDal.createReview(comment, rating);
-      res.status(200).json("envio ok");
+      const { comment, rating, event_id } = req.body;
+      await reviewsDal.createReview(comment, rating, event_id);
+      res.status(200).json({ message: "Reseña enviada correctamente" });
     } catch (error) {
-      res.status(500).json({message:"Error de server"});
+      res.status(500).json({ message:"Error de server" });
     }
   }
 }
--- server/modules/reviews/reviews.dal.js
+++ server/modules/reviews/reviews.dal.js
@@ -1,10 +1,10 @@
-import executeQuery from "../../config/db";
+import executeQuery from "../../config/db.js";
 
 class ReviewDal {
-  createReview = async(comment, rating) => {
+  createReview = async(comment, rating, event_id) => {
     try {
-      let sql = "INSERT INTO review (rating, description) VALUES (?, ?)";
-      let values = [rating, comment];
+      let sql = "INSERT INTO review (rating, description, event_id) VALUES (?, ?, ?)";
+      let values = [rating, comment, event_id];
       const result = await executeQuery(sql, values);
     } catch (error) {
       throw { message: "Error en base de datos" };
