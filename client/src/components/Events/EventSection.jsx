c349393d8a1a9183352f04e13137f1a249aaacad
Amaia <amaiavzqz@gmail.com>
2025-09-02 10:50:20 +0200

    cambios varios

--- client/src/components/Events/EventSection.jsx
+++ client/src/components/Events/EventSection.jsx
@@ -1,55 +1,40 @@
 const EventSection = ({ section, index , timeEvent }) => {
- console.log("----", section);
-
-  //if (!section) return null
-
   const isFirst = index === 0
   const isEven = index % 2 === 0
   const orientationClass = isEven ? 'flex-md-row' : 'flex-md-row-reverse'
-  console.log(section.key_points)
 
   return (
-    <section
-      className={`event-section ${isEven ? 'even-section' : 'odd-section'}`}
-    >
+    <section className={`event-section ${isEven ? 'even-section' : 'odd-section'}`}>
       <h3 className="section-title">
         {isFirst ? 'Público beneficiario' : section.section_title}
       </h3>
 
-      <div
-        className={`section-body d-flex flex-column ${orientationClass} gap-4`}
-      >
-        {section.images && section.images.length > 0 && (
-          <div className="flex-fill section-gallery">
-            {section.images.map((file, idx) => (
-              <img
-                key={idx}
-                src={`${
-                  import.meta.env.VITE_SERVER_URL_PUBLIC
-                }images/events/${file}`}
-                alt=""
-                className="section-img"
-              />
-            ))}
-          </div>
-        )}
+      <div className={`section-body d-flex flex-column ${orientationClass} gap-4`}>
+        <div className="flex-fill section-gallery">
+          {section.images.map((imagen, idx) => (
+            <img
+              key={idx}
+              src={`${import.meta.env.VITE_SERVER_URL_PUBLIC}images/events/${imagen.file}`}
+              alt=""
+              className="section-img"
+            />
+          ))}
+        </div>
+        
         <div className="flex-fill">
           {!isFirst && section.section_subtitle && (
             <h4 className="section-subtitle">{section.section_subtitle}</h4>
           )}
 
           <p className="section-text">{section.section_description}</p>
-
-          
-            <ul className="section-keypoints">
-              {section.keyPoints.map((kp) => (
-                <li key={kp.section_key_point_id}>
-                  <strong>{kp.key_point_title}:</strong>{' '}
-                  {kp.key_point_description}
-                </li>
-              ))}
-            </ul>
-         
+          <ul className="section-keypoints">
+            {section.keyPoints.map((kp) => (
+              <li key={kp.section_key_point_id}>
+                <strong>{kp.key_point_title}:</strong>{' '}
+                {kp.key_point_description}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </section>
